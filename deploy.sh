eb30da84d6695486d16f723eaeb49c524b0effb6
s-abe0 <shane.abe0@gmail.com>
2019-10-31 15:57:04 -0400

    few updates

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 sudo chmod 400 Ansible-Test.pem
 
 ansible-playbook -i inventory prepare.yml --key-file Ansible-Test.pem
-ansible-playbook -i inventory openshift-ansible/playbooks/prerequisites.yml --key-file Ansible-Test.pem
-ansible-playbook -i inventory openshift-ansible/playbooks/deploy_cluster.yml --key-file Ansible-Test.pem
+ansible-playbook -i inventory ../openshift-ansible/playbooks/prerequisites.yml --key-file Ansible-Test.pem
+ansible-playbook -i inventory ../openshift-ansible/playbooks/deploy_cluster.yml --key-file Ansible-Test.pem
